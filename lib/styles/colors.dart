fcedb7ebd4a3442216823d3968a3956753363732
karelbondan <karel.herdito@binus.ac.id>
2024-04-13 19:51:30 +1000

    Added new colours

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class CustomColor {
   static const Color mainBrown = Color.fromARGB(255, 69, 57, 40);
+  static const Color buttonOrange = Color.fromARGB(255, 216, 91, 0);
+  static const Color buttonYellow = Color.fromARGB(255, 227, 162, 0);
 }
