0d64712f08c6c5c4671c793a1bcbf99116d8e8d0
karelbondan <karel.herdito@binus.ac.id>
2024-04-16 01:57:38 +1000

    Can now track route history; Implemented method to pop current route

--- lib/utility/activity.dart
+++ lib/utility/activity.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Activity {
+  static List<Widget> history = [];
+
   static Future<Widget> _buildPageAsync(
       BuildContext context, Widget activity) async {
     return Future.microtask(() {
@@ -8,13 +10,18 @@ class Activity {
     });
   }
 
+  static void finishActivity(BuildContext context,
+      [bool systemBackPressed = false]) {
+    history.removeLast();
+    if (!systemBackPressed) {
+      Navigator.of(context).pop();
+    }
+  }
+
   static void startActivity(BuildContext context, Widget activity,
       [int delay = 300]) {
     Future.delayed(Duration(milliseconds: delay), () async {
-      // Navigator.push(
-      //   context,
-      //   MaterialPageRoute(builder: (context) => activity),
-      // );
+      history.add(activity);
       var page = await _buildPageAsync(context, activity);
       var route = MaterialPageRoute(builder: (_) => page);
       Navigator.push(context, route);
