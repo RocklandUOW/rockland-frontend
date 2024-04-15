b15b7fb789a4b0915a23c4f1b8cf284f04ef093d
karelbondan <karel.herdito@binus.ac.id>
2024-04-16 01:58:04 +1000

    Added a new custom colour

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -4,4 +4,5 @@ class CustomColor {
   static const Color mainBrown = Color.fromARGB(255, 69, 57, 40);
   static const Color buttonOrange = Color.fromARGB(255, 216, 91, 0);
   static const Color buttonYellow = Color.fromARGB(255, 227, 162, 0);
+  static const Color lightRed = Color.fromARGB(255, 246, 103, 93);
 }
