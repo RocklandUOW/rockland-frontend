3e2cb1423a43d67d9f20f60503cf9d5a2c18a597
karelbondan <karel.herdito@binus.ac.id>
2024-05-10 17:43:35 +1000

    Almost fully styled discover page; Added loading indicator to load new posts in profile page; Added discover post thumbnail template

--- lib/pages/home/discover.dart
+++ lib/pages/home/discover.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:rockland/components/post_thumbnail_discover.dart';
+import 'package:rockland/components/textfield.dart';
 import 'package:rockland/styles/colors.dart';
 import 'package:rockland/utility/common.dart';
 
@@ -30,8 +32,7 @@ class _DiscoverPageState extends State<DiscoverPage> {
   void initState() {
     super.initState();
     widget.controller!.getListController = getListController;
-    widget.controller!.setListControllerPosition =
-        setListControllerPosition;
+    widget.controller!.setListControllerPosition = setListControllerPosition;
   }
 
   ScrollController getListController() {
@@ -45,17 +46,55 @@ class _DiscoverPageState extends State<DiscoverPage> {
 
   @override
   Widget build(BuildContext context) {
+    double parentWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: CustomColor.mainBrown,
       body: SafeArea(
-          child: RefreshIndicator(
+          child: Stack(
+        children: [
+          RefreshIndicator(
               onRefresh: onRefresh,
               child: ListView.builder(
-                itemCount: 25,
+                itemCount: 10 + 2,
                 itemBuilder: (context, index) {
-                  return Text("data");
+                  if (index == 0) {
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 90),
+                      child: Container(
+                        height: 1,
+                        width: parentWidth,
+                        color: Colors.white.withAlpha(50),
+                      ),
+                    );
+                  } else if (index < 10 + 2 - 1) {
+                    return const PostThumbnailDiscover();
+                  } else {
+                    return const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 25),
+                      child: Center(
+                        child: SizedBox(
+                          height: 18,
+                          width: 18,
+                          child: CircularProgressIndicator(
+                            color: CustomColor.extremelyLightBrown,
+                          ),
+                        ),
+                      ),
+                    );
+                  }
                 },
-              ))),
+              )),
+          const TextField(
+            decoration: InputDecoration(
+                hintText: "Search",
+                hintStyle: TextStyle(
+                    fontWeight: FontWeight.normal, color: Colors.white30),
+                focusColor: Colors.black,
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white))),
+          ),
+        ],
+      )),
     );
   }
 }
